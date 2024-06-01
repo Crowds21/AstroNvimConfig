ab6827cac08316ba66d75ebfd84d3c0ccb8e0f3c
Crowds21 <mingqing98.163.com>
2024-06-01 18:08:42 +0800

    notify,mappings,colorscheme,community,neotree

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,46 @@
+return {
+  -- AstroCore
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- Open config files
+          ["<Leader>__"] ={"<cmd>e ~/.config/nvim/lua/plugins/user.lua<cr>"}
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+  -- Astrolsp
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          K = {
+            function()
+              vim.lsp.buf.hover()
+            end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function()
+              vim.lsp.buf.declaration()
+            end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
+}
